93be980ba13739a553b975eb0dd8f1b0344e5d1c
Gobind Puniani <gobindpuniani@gmail.com>
2021-03-01 01:54:11 -0800

    Initial Commit

---
+++ StroopBrainGame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StroopBrainGame
+//
+//  Created by Gobind Puniani on 3/1/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StroopBrainGame/StroopBrainGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  StroopBrainGameApp.swift
+//  StroopBrainGame
+//
+//  Created by Gobind Puniani on 3/1/21.
+//
+
+import SwiftUI
+
+@main
+struct StroopBrainGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
